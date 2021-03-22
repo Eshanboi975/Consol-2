dc2a4f651823bf862532ec828d2d6d83c7347614
Eshanboi975 <81173589+Eshanboi975@users.noreply.github.com>
2021-03-22 23:04:50 +0500

    Add files via upload
    I made a simple example of a console prepared.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Forgone";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WindowHeight = 40;
+
+            int num01 = 5;
+            int num02 = 10;
+            int num03 = 15;
+
+            Console.WriteLine("Hello what's your name?");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Hello " + userName + ", nice to meet you.");
+            Console.WriteLine("My name is YR-5000.\nI'm an AI sent from the future to destroy mankind.");
+            Console.WriteLine("What's your favourite color?");
+
+            string colorName = Console.ReadLine();
+
+            Console.WriteLine("I like " + colorName + ", Its Awesome.");
+            Console.WriteLine("What is your age?");
+
+            string age = Console.ReadLine();
+
+            Console.WriteLine("So, you're " + age + "years old.");
+            Console.WriteLine("Credits:\nProgrammer: Muhammad Eshan\nCreator: Muhammad Eshan\nAgeofCreatorandProgrammer: 9, thank you for using my console(^o^).");
+
+            Console.ReadKey();
+        }
+    }
+}
